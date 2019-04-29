67c80b3fb49bafa39be88b1583cfa1a9f9f98528
Brandon Liang <54skyxenon@gmail.com>
2019-04-29 01:35:43 -0700

    Added ability to view details for menu items

--- Food-Tracker/DetailViewController.swift
+++ Food-Tracker/DetailViewController.swift
@@ -8,14 +8,44 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-    func configureView() {
+class DetailViewController: UIViewController
+{
+    
+    //@IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var costLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UITextField!
+    
+    func configureView()
+    {
         // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
+        if let detail = detailItem
+        {
+            if let label = titleLabel
+            {
+                label.text = detail.title
+            }
+            if let label = timestampLabel
+            {
+                label.text = detail.timestamp
+            }
+            if let label = countryLabel
+            {
+                label.text = detail.country
+            }
+            if let label = typeLabel
+            {
+                label.text = detail.getTypeOfFood()
+            }
+            if let label = costLabel
+            {
+                label.text = "$" + String(detail.cost)
+            }
+            if let label = descriptionLabel
+            {
                 label.text = detail.description
             }
         }
@@ -27,7 +57,7 @@ class DetailViewController: UIViewController {
         configureView()
     }
 
-    var detailItem: NSDate? {
+    var detailItem: MenuItem? {
         didSet {
             // Update the view.
             configureView()
--- Food-Tracker/MasterViewController.swift
+++ Food-Tracker/MasterViewController.swift
@@ -3,15 +3,15 @@
 //  Food-Tracker
 //
 //  Created by Brandon Liang on 4/28/19.
-//  Copyright © 2019 Brandon Liang. All rights reserved.
+//  Copyright ©2019 Brandon Liang. All rights reserved.
 //
 
 import UIKit
 
 var items = [MenuItem]()
 
-class MasterViewController: UITableViewController {
-
+class MasterViewController: UITableViewController
+{
     var detailViewController: DetailViewController? = nil
     var objects = [Any]()
     
@@ -22,31 +22,35 @@ class MasterViewController: UITableViewController {
 
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(switchScreens(_:)))
         navigationItem.rightBarButtonItem = addButton
-        if let split = splitViewController {
+        if let split = splitViewController
+        {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
     }
 
-    override func viewWillAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool)
+    {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
     }
 
     @objc
-    func switchScreens(_ sender: Any) {
+    func switchScreens(_ sender: Any)
+    {
         performSegue(withIdentifier: "new", sender: self)
-        /*objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)*/
     }
 
     // MARK: - Segues
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate //this is what controlling the segue to the detailed view
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.identifier == "showDetail"
+        {
+            if let indexPath = tableView.indexPathForSelectedRow
+            {
+                let object = items[indexPath.row]
+                //this is what controlling the segue to the detailed view
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
@@ -57,36 +61,37 @@ class MasterViewController: UITableViewController {
 
     // MARK: - Table View
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return items.count //return objects.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = items[indexPath.row] //let object = objects[indexPath.row] as! NSDate
+        cell.textLabel!.text = object.title
         return cell
     }
 
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool
+    {
         // Return false if you do not want the specified item to be editable.
         return true
     }
 
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == .delete
+        {
+            items.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
-
-
 }
 
