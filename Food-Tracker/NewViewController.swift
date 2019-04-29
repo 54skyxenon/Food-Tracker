8c28ea454dd68bf6e3c54fae76954d41301cef65
Brandon Liang <54skyxenon@gmail.com>
2019-04-28 23:28:11 -0700

    Connected Label UIs to code

--- Food-Tracker/NewViewController.swift
+++ Food-Tracker/NewViewController.swift
@@ -8,15 +8,41 @@
 
 import UIKit
 
-class NewViewController: UIViewController
+class NewViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
 {
-    let typesOfItems = ["Entree", "Drink","Dessert","Appetizer"]
+    let typesOfItems = ["Entree", "Drink", "Dessert", "Appetizer"]
     
-    @IBAction func createDishButton(_ sender: Any) {
-        performSegue(withIdentifier: "backAfterCreateDish", sender: self) // should be the last thing
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var dishNameLabel: UITextField!
+    @IBOutlet weak var timePicker: UIDatePicker!
+    @IBOutlet weak var countryOfOriginLabel: UITextField!
+    @IBOutlet weak var costLabel: UITextField!
+    @IBOutlet weak var descriptionLabel: UITextView!
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return typesOfItems.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return typesOfItems[row]
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pickerView.delegate = self
+        pickerView.dataSource = self
     }
+    
+    // MARK: Segues
+    
+    @IBAction func createDishButton(_ sender: Any) {
+        performSegue(withIdentifier: "backAfterCreateDish", sender: self)
+    }
+    
+    // MARK: Data-processing functions
+    
 }
